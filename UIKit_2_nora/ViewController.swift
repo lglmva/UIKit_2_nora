2862c0138e9e41160fc40e3e4076b717f66c62bb
lglmva <62218130+lglmva@users.noreply.github.com>
2023-03-05 10:58:47 +0300

    Initial Commit

---
+++ UIKit_2_nora/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit_2_nora
+//
+//  Created by Oleg Lomov on 05/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
